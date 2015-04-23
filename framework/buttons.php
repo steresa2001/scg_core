66d713baadbb7f06d426d6de2307ae756305a911
Steven Gore <steresa2001@yahoo.com>
2015-04-22 20:11:16 -0400

    updated framework

--- framework/buttons.php
+++ framework/buttons.php
@@ -121,6 +121,43 @@
 
 			<section>
 				<div class="row">
+					<h2>Buttons With Icons</h2>
+					<div class="showcase">
+						<p>If you ever heard yourself saying "there is nothing like a nice shiny button with a nice fancy icon just begging to pe clicked", no need to look any further.</p>
+					</div>
+				</div>
+
+				<div class="row">
+					<a class="default-btn glyphicons wifi" href="#">Join Network</a>
+					<a class="default-btn glyphicons user" href="#">My Account</a>
+					<a class="default-btn glyphicons calendar" href="#">Choose Date</a>
+					<a class="default-btn glyphicons chat" href="#">Chat Now</a>
+					<a class="default-btn glyphicons charts" href="#">See Data</a>
+					<a class="default-btn glyphicons lock" href="#">Secure Login</a>
+				</div>
+
+				<div class="row">
+					<a class="default-btn" href="#">My Favorite <span class="glyphicons glyphicons heart"></span></a>
+					<a class="default-btn" href="#">All Set <span class="glyphicons ok"></span></a>
+					<a class="default-btn" href="#">My Settings <span class="glyphicons cogwheel"></span></a>
+					<a class="default-btn" href="#">Need Help <span class="glyphicons circle_question_mark"></span></a>
+					<a class="default-btn" href="#">My Cart <span class="glyphicons shopping_cart"></span></a>
+					<a class="default-btn " href="#">My Waether <span class="glyphicons sun"></span></a>
+				</div>
+
+				<div class="row">
+					<script type="syntaxhighlighter" class="brush: js ctns-24"><![CDATA[
+		  				<a class="simple-btn-xl btn-radius-all" href="#">/* Rounded button all sides */</a>
+						<a class="simple-btn-xl btn-radius-left-side" href="#">/* Rounded button left side */</a>
+						<a class="simple-btn-xl btn-radius-right-side" href="#">/* Rounded button right side */</a>\
+						<a class="simple-btn-xl btn-radius-top" href="#">/* Rounded button top */</a>
+						<a class="simple-btn-xl btn-radius-bottom" href="#">/* Rounded button bottom */</a>
+					]]></script>
+				</div>
+			</section>
+
+			<section>
+				<div class="row">
 					<h2>Buttons For Forms</h2>
 					<div class="showcase">
 						<p>The default browser style for a true button is not that appealing. You can easily customize this by using the same options we provided above. The only difference is in the markup. Rather than using an anchor tag, use the button tag instead.</p>
