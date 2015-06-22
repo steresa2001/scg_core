920f0b5db83b5cf7b6fe68caca778e60ccd633da
Steven Gore <steresa2001@yahoo.com>
2015-06-21 20:18:54 -0400

    added to the content page

--- framework/type.php
+++ framework/type.php
@@ -81,16 +81,34 @@
 						<p>Ohh, just look at those beautiful paragraphs. Paragraphs have built in line height and padding to allow for a more enjoyable reading experience.</p>
 					</div>
 				</div>
-
+				
+				<h4>Left Aligned</h4>
 				<div class="row no-style">
-					<p>What is real? How do you define 'real'? If you're talking about what you can feel, what you can smell, what you can taste and see, then 'real' is simply electrical signals interpreted by your brain.</p>
-					<p>Have you ever had a dream, Neo, that you were so sure was real? What if you were unable to wake from that dream? How would you know the difference between the dream world and the real world?</p>
-					<p>I see it in your eyes. You have the look of a man who accepts what he sees because he is expecting to wake up. Ironically, that's not far from the truth. Do you believe in fate, Neo?</p>
+					<p class="left-aligned">What is real? How do you define 'real'? If you're talking about what you can feel, what you can smell, what you can taste and see, then 'real' is simply electrical signals interpreted by your brain.</p>
+				</div>
+
+				<h4>Right Aligned</h4>
+				<div class="row no-style">
+					<p class="right-aligned">Have you ever had a dream, Neo, that you were so sure was real? What if you were unable to wake from that dream? How would you know the difference between the dream world and the real world?</p>
+				</div>
+
+				<h4>Center Aligned</h4>
+				<div class="row no-style">
+					<p class="centered-aligned">I see it in your eyes. You have the look of a man who accepts what he sees because he is expecting to wake up. Ironically, that's not far from the truth. Do you believe in fate, Neo?</p>
+				</div>
+
+				<h4>Justified</h4>
+				<div class="row no-style">
+					<p class="justified">This is your last chance. After this, there is no turning back. You take the blue pill - the story ends, you wake up in your bed and believe whatever you want to believe. 
+					You take the red pill - you stay in Wonderland and I show you how deep the rabbit-hole goes.</p>
 				</div>
 
 				<div class="row">
 					<script type="syntaxhighlighter" class="brush: js ctns-24"><![CDATA[
-						<p>/*Your content*/</p>
+						<p class="left-aligned">/*Your content*/</p>
+						<p class="right-aligned">/*Your content*/</p>
+						<p class="centered-aligned">/*Your content*/</p>
+						<p class="justified">/*Your content*/</p>
 					]]></script>
 				</div>
 			</section>
@@ -180,25 +198,25 @@
 				<h4>Ordered List</h4>
 				<div class="row no-style">
 					<ol>
-						<li>list</li>
-						<li>list</li>
-						<li>list
+						<li>Goonies never say die!</li>
+						<li>So... that's why they call you One-Eyed Willie... One-Eyed Willie</li>
+						<li>Ruth! Ruth! Baby! Ruth!
 							<ol class="styled-list-upper-alpha">
-								<li>list</li>
-								<li>list</li>
-								<li>list
+								<li>Run, Forrest! Run!</li>
+								<li>Me and Jenny goes together like peas and carrots</li>
+								<li>I was born with big gums, sir
 									<ol class="styled-list-lower-alpha">
-										<li>list</li>
-										<li>list</li>
-										<li>list
+										<li>Here's to swimmin' with bow-legged women</li>
+										<li>It's only an island if you look at it from the water</li>
+										<li>I'm not going to waste my time arguing with a man who's lining up to be a hot lunch
 											<ol class="styled-list-upper-roman">
-												<li>list</li>
-												<li>list</li>
-												<li>list
+												<li>What are you looking at, nerd?</li>
+												<li>Excuse please, but why do they call you "booger"?</li>
+												<li>You're *jocks*! Go live in the gym!
 													<ol class="styled-list-lower-roman">
-														<li>list</li>
-														<li>list</li>
-														<li>list</li>
+														<li>When you pray for rain, you gotta deal with the mud too</li>
+														<li>I don't have a lot of time. Which means you don't have any</li>
+														<li>Man gotta be the man, fish gotta be the fish</li>
 													</ol>
 												</li>
 											</ol>
@@ -212,17 +230,35 @@
 
 				<div class="row">
 					<script type="syntaxhighlighter" class="brush: js ctns-24"><![CDATA[
-						<ul>
-							<li>/*You couldn't hear a dump truck driving through a nitroglycerin plant*/</li>
-							<li>/*Oh dear, did I break wind?*/</li>
-							<li>/*I'm sorry. This is our family's first kidnapping*/
-								<ul class="unstyled-list">
-									<li>/*Uh oh, fifteen minutes to Judge Wapner*/</li>
-									<li>/*Gotta get my boxer shorts at K-Mart*/</li>
-									<li>/*Of course you can't have pancakes without maple syrup*/</li>
-								</ul>
+						<ol>
+							<li>/*Goonies never say die!*/</li>
+							<li>/*So... that's why they call you One-Eyed Willie... One-Eyed Willie*/</li>
+							<li>/*Ruth! Ruth! Baby! Ruth!*/
+								<ol class="styled-list-upper-alpha">
+									<li>/*Run, Forrest! Run!*/</li>
+									<li>/*Me and Jenny goes together like peas and carrots*/</li>
+									<li>/*I was born with big gums, sir*/
+										<ol class="styled-list-lower-alpha">
+											<li>/*Here's to swimmin' with bow-legged women*/</li>
+											<li>/*It's only an island if you look at it from the water*/</li>
+											<li>/*I'm not going to waste my time arguing with a man who's lining up to be a hot lunch*/
+												<ol class="styled-list-upper-roman">
+													<li>/*What are you looking at, nerd?*/</li>
+													<li>/*Excuse please, but why do they call you "booger"?*/</li>
+													<li>/*You're *jocks*! Go live in the gym!*/
+														<ol class="styled-list-lower-roman">
+															<li>/*When you pray for rain, you gotta deal with the mud too*/</li>
+															<li>/*I don't have a lot of time. Which means you don't have any*/</li>
+															<li>/*Man gotta be the man, fish gotta be the fish*/</li>
+														</ol>
+													</li>
+												</ol>
+											</li>
+										</ol>
+									</li>
+								</ol>
 							</li>
-						</ul>
+						</ol>
 					]]></script>
 				</div>
 			</section>
