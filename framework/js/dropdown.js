091947b32da1715d80446e9e337c881a433ffbe5
Steven Gore <steresa2001@yahoo.com>
2015-08-29 20:28:40 -0400

    finished js for dropdown and added styles

--- framework/js/dropdown.js
+++ framework/js/dropdown.js
@@ -1,15 +1,18 @@
 $(document).ready(function(){
     // Dropdown menu
     var findDropdowns = document.querySelectorAll(".has-dropdown");
-    var toArray = function(collection) {return [].slice.call(collection)}
-
+    var toArray = function(collection) {return [].slice.call(collection);}
+     $(document).on('click', function(e) {
+        $("ul").removeClass("show");
+    });
     toArray(findDropdowns).forEach(function(e) {
         e.addEventListener("click", function(ev) {
             ev.preventDefault();
-          
+            ev.stopPropagation();
+            
             var others = toArray(document.querySelectorAll(".show"));
             e.parentNode.children[1].classList.add("show");
-            others.forEach(function(o){
+            others.forEach(function(){
                 $(others).toggleClass("show");
             });
         }, false);
--- framework/navigation.php
+++ framework/navigation.php
@@ -30,78 +30,53 @@
 				</div>
 
 				<div class="row no-style">
-					<ul class="nav">
-						<li><a class="has-dropdown" href="">Main Link</a>
-							<ul>
-								<li><a href="">Dropdown-Link</a></li>
-								<li><a href="">Dropdown-Link</a></li>
-								<li><a href="">Dropdown-Link</a></li>
-							</ul>
-						</li>
-						<li><a class="has-dropdown" href="">Main Link</a>
-							<ul>
-								<li><a href="">Dropdown-Link</a></li>
-								<li><a href="">Dropdown-Link</a></li>
-								<li><a href="">Dropdown-Link</a></li>
-							</ul>
-						</li>
-						<li><a class="has-dropdown" href="">Main Link</a>
-							<ul>
-								<li><a href="">Dropdown-Link</a></li>
-								<li><a href="">Dropdown-Link</a></li>
-								<li><a href="">Dropdown-Link</a></li>
-							</ul>
-						</li>
-						<li><a class="has-dropdown" href="">Main Link</a>
-							<ul>
-								<li><a href="">Dropdown-Link</a></li>
-								<li><a href="">Dropdown-Link</a></li>
-								<li><a href="">Dropdown-Link</a></li>
-							</ul>
-						</li>
-						<li><a class="has-dropdown" href="">Main Link</a>
-							<ul>
-								<li><a href="">Dropdown-Link</a></li>
-								<li><a href="">Dropdown-Link</a></li>
-								<li><a href="">Dropdown-Link</a></li>
-							</ul>
-						</li>
-						<li><a class="has-dropdown" href="">Main Link</a>
-							<ul>
-								<li><a href="">Dropdown-Link</a></li>
-								<li><a href="">Dropdown-Link</a></li>
-								<li><a href="">Dropdown-Link</a></li>
-							</ul>
-						</li>
-					</ul>
-				</div>
-
-				<!-- <div class="row no-style">
-					<ul class="nav">
-						<li class="has-dropdown"><a href="">Main Link</a>
-							<ul class="dopdown-menu">
-								<li class="has-dropdown"><a href="">Dropdown-Link</a>
-									<ul class="dopdown-menu">
-										<li><a href="">Sub-Dropdown-Link</a>
-									</ul>
-								</li>
-								<li><a href="">Dropdown-Link</a></li>
-								<li><a href="">Dropdown-Link</a></li>
-							</ul>
-						</li>
-						<li class="has-dropdown"><a href="">Main Link</a>
-							<ul class="dopdown-menu">
-								<li class="has-dropdown"><a href="">Dropdown-Link</a>
-									<ul class="dopdown-menu">
-										<li><a href="">Sub-Dropdown-Link</a>
-									</ul>
-								</li>
-								<li><a href="">Dropdown-Link</a></li>
-								<li><a href="">Dropdown-Link</a></li>
-							</ul>
-						</li>
-					</ul>
-				</div> -->
+					<nav class="main">
+						<ul class="sub-nav inline">
+							<li><a class="has-dropdown" href="">Main Link</a>
+								<ul>
+									<li><a href="">Dropdown-Link</a></li>
+									<li><a href="">Dropdown-Link</a></li>
+									<li><a href="">Dropdown-Link</a></li>
+								</ul>
+							</li>
+							<li><a class="has-dropdown" href="">Main Link</a>
+								<ul>
+									<li><a href="">Dropdown-Link</a></li>
+									<li><a href="">Dropdown-Link</a></li>
+									<li><a href="">Dropdown-Link</a></li>
+								</ul>
+							</li>
+							<li><a class="has-dropdown" href="">Main Link</a>
+								<ul>
+									<li><a href="">Dropdown-Link</a></li>
+									<li><a href="">Dropdown-Link</a></li>
+									<li><a href="">Dropdown-Link</a></li>
+								</ul>
+							</li>
+							<li><a class="has-dropdown" href="">Main Link</a>
+								<ul>
+									<li><a href="">Dropdown-Link</a></li>
+									<li><a href="">Dropdown-Link</a></li>
+									<li><a href="">Dropdown-Link</a></li>
+								</ul>
+							</li>
+							<li><a class="has-dropdown" href="">Main Link</a>
+								<ul>
+									<li><a href="">Dropdown-Link</a></li>
+									<li><a href="">Dropdown-Link</a></li>
+									<li><a href="">Dropdown-Link</a></li>
+								</ul>
+							</li>
+							<li><a class="has-dropdown" href="">Main Link</a>
+								<ul>
+									<li><a href="">Dropdown-Link</a></li>
+									<li><a href="">Dropdown-Link</a></li>
+									<li><a href="">Dropdown-Link</a></li>
+								</ul>
+							</li>
+						</ul>
+					</div>
+				</nav>
 
 				<div class="row">
 					<script type="syntaxhighlighter" class="brush: js ctns-24"><![CDATA[
